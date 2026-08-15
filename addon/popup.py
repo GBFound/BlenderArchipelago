9dd3ed23e24656de2c53f0aeaeb1376459c4942b
GBFound <gigabytesfound@gmail.com>
2026-08-15 03:12:04 -0500

    Add popup.py for real

---
+++ addon/popup.py
@@ -0,0 +1,33 @@
+import bpy
+import collections
+
+_popups = collections.deque()
+_show_next = True
+
+
+def enqueue(message: str):
+    _popups.append(message)
+
+    if _show_next:
+        _schedule()
+
+
+def show_next():
+    global _show_next
+    _show_next = True
+    
+    if _popups:
+        _schedule()
+
+
+def _schedule():
+    global _show_next
+    _show_next = False
+
+    message = _popups.popleft()
+    bpy.app.timers.register(
+        # Use a timer to defer the call until context is available.
+        # Returning None stops the timer from repeating
+        lambda: bpy.ops.ap.popup("INVOKE_DEFAULT", message=message) and None,
+    )
+    print(f"[Blender AP] {message}")
